30e4d1779ac35073931ff06311fad068213ec33b
MWaleedK <waleedjaved911@gmail.com>
2026-07-13 14:27:34 +0500

    dotenv file loading arguments added

--- mysite/settings.py
+++ mysite/settings.py
@@ -15,12 +15,13 @@ from pathlib import Path
 from dotenv import load_dotenv
 
 
-# Load the .env file's data
-load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Load the .env file's data
+load_dotenv(BASE_DIR / ".env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
