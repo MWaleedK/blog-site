bf85b4b7698fbe75addb2d428806982a6ce824dd
MWaleedK <waleedjaved911@gmail.com>
2026-07-16 19:53:58 +0500

    Made changes to the mysite/url ordering of urlpatterns

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views
@@ -23,11 +24,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/login/', views.LoginView.as_view(), name='login'),
-    path('accounts/logout/',views.LogoutView.as_view(next_page='/'), name = 'logout'),
-    path("blog/",include("blog.urls")),
-    path('',include(wagtail_urls)),
-    path('cms/',include(wagtailadmin_urls)),
-    path('pages/',include(wagtail_urls)),
+    path("admin/", admin.site.urls),
+    path("accounts/login/", views.LoginView.as_view(), name="login"),
+    path("accounts/logout/", views.LogoutView.as_view(next_page="/"), name="logout"),
+    path("blog/", include("blog.urls")),
+    path("cms/", include(wagtailadmin_urls)),
+    path("pages/", include(wagtail_urls)),
+    path("", include(wagtail_urls)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
